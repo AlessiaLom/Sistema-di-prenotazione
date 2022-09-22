3f1b18db302e14e85f2b909147a6b61952472b5e
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2022-09-22 10:03:35 +0200

    add openSidebar btn on services page

--- managementPage/client/src/components/services/Services.js
+++ managementPage/client/src/components/services/Services.js
@@ -1,8 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from "react";
-import "./../../styles/pages.css"
 import {GoogleOAuthProvider} from '@react-oauth/google';
 import {MyGoogleLogin} from "./MyGoogleLogin";
+
+let isSideOpen = true;
 
 export default class Services extends React.Component {
     constructor(props) {
@@ -10,8 +11,24 @@ export default class Services extends React.Component {
         this.clientId = '504181834497-omrl5mnes3qmvvu39hu5v404lemlfq1c.apps.googleusercontent.com'
     }
 
+    handleSideBarChange(){
+        let sideBar = document.querySelector("#sidebarDiv");
+        let main = document.querySelector("#mainContentContainer");
+        if(isSideOpen){
+            sideBar.removeAttribute("style");
+            main.setAttribute("style", "marginLeft: 0");
+            isSideOpen = false;
+        } else {
+            sideBar.setAttribute("style", "display: block;");
+            main.setAttribute("style", "marginLeft: 250px");
+            isSideOpen = true;
+        }
+    }
+
     render() {
         return (
+            <>
+            <button className='openbtn' onClick={this.handleSideBarChange}>&#9776;</button>
             <div>
                 <GoogleOAuthProvider clientId={this.clientId}>
                     <MyGoogleLogin
@@ -21,6 +38,7 @@ export default class Services extends React.Component {
                         onLogout={this.props.onLogout}/>
                 </GoogleOAuthProvider>
             </div>
+            </>
         );
     }
 
