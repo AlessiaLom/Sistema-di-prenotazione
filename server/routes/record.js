d19d10d8ef3e1cb020c28ddf8ced423f5d2b2812
Gianluca Zani <zani.gianluca98@gmail.com>
2022-09-23 15:50:12 +0200

    fixed canceling

--- server/routes/record.js
+++ server/routes/record.js
@@ -262,12 +262,8 @@ recordRoutes.route("/bookings/seats/:id/:day/:activity").get(async function (req
     let respMessage = {};
     let seats = 0;
     let day = new Date(request.params.day);
-    console.log(day)
     let date = day.getFullYear() + '-' + (day.getMonth() + 1) + '-' + day.getDate();
     let activity = request.params.activity;
-
-    console.log(date)
-    console.log(activity)
 
     bookingArray.bookings.forEach((booking) => {
         if (booking.bookingDate === date && booking.bookingActivity === activity && booking.bookingStatus === 'confirmed') {
